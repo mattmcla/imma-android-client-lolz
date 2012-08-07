9f0f9b6122390a3dd47689fe6396f5f3ea7f0a9c
Matt McLaughlin <mclaughlin.matt@gmail.com>
2012-08-06 22:43:28 -0700

    First Commit

---
+++ src/com/playhaven/imma/client/lolz2/CreateGame.java
@@ -0,0 +1,43 @@
+package com.playhaven.imma.client.lolz2;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v4.app.NavUtils;
+
+public class CreateGame extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_game);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_create_game, menu);
+        return true;
+    }
+
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+        	case R.id.cancel_game:
+            case android.R.id.home:
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+            case R.id.save_game:
+            	saveGame();
+            	return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    
+    public void saveGame() {
+    	return;
+    }
+
+}
---
+++ src/com/playhaven/imma/client/lolz2/MainActivity.java
@@ -0,0 +1,38 @@
+package com.playhaven.imma.client.lolz2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+    	switch(item.getItemId()){
+    	case R.id.menu_add_game:
+    		addGame();
+    		return true;
+    	default:
+    		return super.onOptionsItemSelected(item);
+    	}
+    }
+    
+    public void addGame(){
+    	Intent intent = new Intent(this, CreateGame.class);
+    	startActivity(intent);
+    }
+}
